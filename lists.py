3f8fb2fc6f0d85adeb722f4d3cc967d05ab88391
Dylan C <97618189+Shoko1321@users.noreply.github.com>
2022-09-01 16:22:26 -0400

    beans

--- lists.py
+++ lists.py
@@ -1,3 +1,6 @@
+
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -7,8 +10,7 @@ class Node:
     __repr__ = __str__
 
 
-node1 = Node(data= 0)
-node2 = Node(data= 0)
+
 
 
 
@@ -16,8 +18,20 @@ class mylist:
     def __init__(self):
         self.head = None
 
-    def __getitem__(self,data):
-        pass
+    def __getitem__(self,index):
+        if isinstance(index, int):
+            if index < 0:
+                index = len(self) + index
+        elif isinstance(index, slice):
+            return[self[i] for i in range(len(self))[index]]
+        else:
+            raise ValueError(f'linked list cannot be indexed with values of type {type(index)}')
+        
+        
+        
+        # cur = self.head
+        # count = 0
+        # while cur 
         
 
     def append(self,data):
@@ -30,7 +44,9 @@ class mylist:
                 last = last.next
             last.next = new_node
             
-                
+
+    def __len__(self):
+        len([1,2,3,4])     
         
     def __str__(self):
         ret = '<'
@@ -45,7 +61,9 @@ class mylist:
 
 
 
-    def pop_at_index(self,position):
+    def pop(self,position=None):
+
+        if not position: position = len(self)-1
  
         index = 0
         if self.head is None:
@@ -67,18 +85,18 @@ class mylist:
             previous.next = current.next       
         print(current)
     
-    def pop_back(self):
-        if(self.head != None):
-            if(self.head.next == None):
-                self.head = None
-            else:
-                temp = self.head
-                while(temp.next.next != None):
-                    temp = temp.next
-                last_Node = temp.next
-                temp.next = None
-                print(last_Node)
-                last_Node = None
+    # def pop_back(self):
+    #     if(self.head != None):
+    #         if(self.head.next == None):
+    #             self.head = None
+    #         else:
+    #             temp = self.head
+    #             while(temp.next.next != None):
+    #                 temp = temp.next
+    #             last_Node = temp.next
+    #             temp.next = None
+    #             print(last_Node)
+    #             last_Node = None
                 
             
         
@@ -116,66 +134,133 @@ class mylist:
         self.head = prev
         
     def clear(self):
-        pass
-    def count(self):
-        pass  
+        while(self.head != None):
+            temp = self.head
+            self.head = self.head.next
+            temp = None
+
+    def count(self,intake):
+        current = self.head
+        count = 0
+        while(current is not None):
+            if current.data == intake:
+                count += 1
+            current = current.next
+        return count
     def copy(self):
-        pass
+
+        current = self.head
+        newList = None
+        tail = None
+
+        while current:
+            if newList is None:
+                newList = Node(current.data, None)
+                tail = newList
+            else:
+                tail.next = Node()
+                tail = tail.next
+                tail.data = current.data
+                tail.next = None
+            current = current.next
+
+        return newList
+        # current = self.head
+        # newList = None
+        # tail = None
+
+        # while current:
+        #     if newList is None:
+        #         newList = Node(current.data, None)
+        #         tail = newList
+        #     else:
+        #         tail.next = Node()
+        #         tail = tail.next
+        #         tail.data = current.data
+        #         tail.next = None
+        #     current = current.next
+        # return newList
+
+        # current = self.head
+        # clone = None
+        # tail = None
+        # while current:
+        #     if clone is None:
+        #         clone = Node(current.data, None)
+        #         tail = clone
+        #     else:
+        #         tail.next = Node()
+        #         tail = tail.next
+        #         tail.data = current.data
+        #         tail.next = None
+        #     current = current.next
+            
     def extend(self):
         pass
     def insert(self):
         pass
     def remove(self):
         pass
+if __name__ == '__main__':
+    node1 = Node(data= 0)
+    node2 = Node(data= 0)
+    x = mylist() 
+    y = []
 
- 
-x = mylist() 
-y = []
+    x.append(3)
+    y.append(3)
+    # print(x,y)
+    x.append(3)
+    y.append(3)
+    # print(x,y)
+    x.append(1)
+    y.append(1)
+    # print(x,y)
+    x.pop(1)
+    y.pop(1)
+    print(x,y)
+    # x.reverse()
+    # y.reverse()
+    # print(x,y)
+    # x.sort()
+    # y.sort()
+    # try:
+    #     print(x.index(2))
+    # except ValueError:
+    #     print("2 is not in xa")
 
-x.append(3)
-y.append(3)
-# print(x,y)
-x.append(2)
-y.append(2)
-# print(x,y)
-x.append(1)
-y.append(1)
-print(x,y)
-# x.pop_at_index(2)
-# y.pop()
-# x.pop_back()
-# print(x,y)
-# x.reverse()
-# y.reverse()
-print(x,y)
-# x.sort()
-# y.sort()
-try:
-    print(x.index(2))
-except ValueError:
-    print("2 is not in xa")
+    # try:
+        
+    #     print(y.index(5))
 
-try:
-    
-    print(y.index(5))
-
-except ValueError:
-    print("5 is no index")
-
-
-
-
+    # except ValueError:
+    #     print("5 is no index")
+    # x.clear()
+    # y.clear()
+    # print(x,y)
+    # print(x.count(3))
+    # print(y.count(3))
+    # print(x.copy())
+    # print(y.copy())
 
 
 
 
 
-# x.pop()
-# y.pop()
-# print(x,y)
-# x.append(x)   x.copy(z)     x.extend(x)    x.insert(x)    x.remove(x)  
-# x.sort(x)  x.clear(x)    x.count(     x.index(x)     x.pop(x) 
-# x.reverse(x)
 
-# as a function
-# linked list
+
+
+
+
+
+
+
+    # x.pop()
+    # y.pop()
+    # print(x,y)
+    # x.append(x)   x.copy(z)     x.extend(x)    x.insert(x)    x.remove(x)  
+    # x.sort(x)  x.clear(x)    x.count(     x.index(x)     x.pop(x) 
+    # x.reverse(x)
+
+    # as a function
+    # linked list
---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+from lists import Node, mylist
+def list_checker(x,y):
+    
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+
+if __name__ == '__main__':
+    unittest.main()
