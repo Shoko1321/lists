6f55be28d98d0eac5b55752b1ab1d7296d2f9f73
Dylan C <97618189+Shoko1321@users.noreply.github.com>
2022-09-13 16:27:46 -0400

    notes

--- lists.py
+++ lists.py
@@ -9,7 +9,18 @@ class Node:
     __repr__ = __str__
 
 
+# # create an iterator object from that iterable
+# iter_obj = iter(iterable)
 
+# # infinite loop
+# while True:
+#     try:
+#         # get the next item
+#         element = next(iter_obj)
+#         # do something with element
+#     except StopIteration:
+#         # if StopIteration is raised, break from loop
+#         break
 
 
 
