77390e384a9493a45f8e8daa61857f3529f2374b
Dylan C <dreamsofgaia1321@gmail.com>
2022-11-28 20:19:04 -0500

    im confused

--- test/add_test.py
+++ test/add_test.py
@@ -1,1 +1,15 @@
+import unittest
+from lists import mylist
 
+class Add_Test(unittest.TestCase):
+    def test_add_1(self):
+        x = []
+        q = []
+        y = mylist()
+        x.append('one'); x.append('two')
+        y.append('one'); y.append('two')
+        q.append('one'); q.append('two')     
+        x = q + x
+        z = y + x
+        self.assertTrue(x == z)
+        print('nuts')   
